ae29a37ab463300c1668f839f3582d1465507c96
Francisco Wilhelm <franciscowilhelm@gmail.com>
2019-06-17 13:11:18 +0200

    updated ICC1

--- ICC1.lme.R
+++ ICC1.lme.R
@@ -5,8 +5,8 @@
 #'Adapted from psychometric package.
 #'
 #'
-#' @param dv variable to calculate the ICC for.
-#' @param grp variable that is the level 2 identifier
+#' @param dv character variable to calculate the ICC for.
+#' @param grp character variable that is the level 2 identifier
 #' @param data dataset to be passed (as dataframe)
 #'
 #' @return ICC value as double
@@ -14,11 +14,13 @@
 #'
 #' @examples
 ICC1.lme <- function (dv, grp, data)  {
-    require(lme4)
+  require(lme4)
   data <- data[, c(dv, grp)]
-    mod <- lmer(dv ~ 1 + (1 | grp), na.action=na.omit)
-    t0 <- as.numeric(VarCorr(mod)[1,1])
-    sig2 <- as.numeric(VarCorr(mod)[2,1])
-    icc1 <- t0/(t0+sig2)
-    return(icc1)
+  names(data) <- c("dv", "grp")
+  mod <- lmer(dv ~ 1 + (1 | grp), na.action=na.omit)
+  t0 <- as.numeric(VarCorr(mod)[1,1])
+  sig2 <- as.numeric(VarCorr(mod)[2,1])
+  icc1 <- t0/(t0+sig2)
+  names(icc1) <- dv
+  return(icc1)
   }
--- testthat.R
+++ testthat.R
@@ -4,3 +4,4 @@ load("exampleData/df_example.RData")
 source("scoreItemsMulti.R")
 library(tidyverse)
 temp <- scoreItemsMulti(scalenames = c("over_h", "ceng"), dataframe = df_example, exclude = TRUE)
+
