93302a22bd55bdc5dfd977d6f2236d0db8a621d3
Francisco Wilhelm <franciscowilhelm@gmail.com>
2019-06-17 13:01:18 +0200

    added ICC1 lme

---
+++ ICC1.lme.R
@@ -0,0 +1,24 @@
+
+
+#' Calculate ICC
+#'
+#'Adapted from psychometric package.
+#'
+#'
+#' @param dv variable to calculate the ICC for.
+#' @param grp variable that is the level 2 identifier
+#' @param data dataset to be passed (as dataframe)
+#'
+#' @return ICC value as double
+#' @export
+#'
+#' @examples
+ICC1.lme <- function (dv, grp, data)  {
+    require(lme4)
+  data <- data[, c(dv, grp)]
+    mod <- lmer(dv ~ 1 + (1 | grp), na.action=na.omit)
+    t0 <- as.numeric(VarCorr(mod)[1,1])
+    sig2 <- as.numeric(VarCorr(mod)[2,1])
+    icc1 <- t0/(t0+sig2)
+    return(icc1)
+  }
