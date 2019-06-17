cdda933beccfed6e11dde7cdd5163b1cc7628bc8
Francisco Wilhelm <franciscowilhelm@gmail.com>
2019-06-17 14:33:19 +0200

    Update ICC1.lme.R

--- ICC1.lme.R
+++ ICC1.lme.R
@@ -17,10 +17,11 @@ ICC1.lme <- function (dv, grp, data)  {
   require(lme4)
   data <- data[, c(dv, grp)]
   names(data) <- c("dv", "grp")
-  mod <- lmer(dv ~ 1 + (1 | grp), na.action=na.omit)
-  t0 <- as.numeric(VarCorr(mod)[1,1])
-  sig2 <- as.numeric(VarCorr(mod)[2,1])
+  mod <- lmer(dv ~ 1 + (1 | grp), data = data, na.action=na.omit)
+  mod_varcorr <- VarCorr(mod)
+  t0 <-  attr(mod_varcorr$grp, "stddev")
+  sig2 <- attr(mod_varcorr, "sc")
   icc1 <- t0/(t0+sig2)
   names(icc1) <- dv
   return(icc1)
-  }
+}
