f14f4a82896f95c0f68c48e1927e1b5d5cd624a0
Francisco Wilhelm <38857150+franciscowilhelm@users.noreply.github.com>
2022-10-28 10:17:54 +0200

    update star_assign

--- star_assign.R
+++ star_assign.R
@@ -3,7 +3,9 @@ star_assign <- function(x) {
     if(x < 0.001) "***"
     else if (x < 0.01) "**"
     else if (x < 0.05) "*"
+    else ""
   }
   else ""
 }
 vstar_assign <- Vectorize(star_assign)
+
